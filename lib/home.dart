b65a7116e5a98c0b1b3ce7bce9eb309efedd3d9f
Adrian-Garcia-Cuya <adrian123575@outlook.com>
2023-03-17 17:43:31 -0500

    Se agrego cartas a la seccion 'vuelos'

--- lib/home.dart
+++ lib/home.dart
@@ -3,7 +3,7 @@ import 'lugares.dart';
 import 'usuarios.dart';
 
 class Home extends StatefulWidget {
-  const Home ({super.key, required this.title});
+  const Home({super.key, required this.title});
 
   final String title;
 
@@ -25,7 +25,7 @@ class HomeState extends State<Home> {
     switch (pos) {
       case 0:
         return const Usuarios();
-      case 1: 
+      case 1:
         return const Lugares();
       default:
         return const Text('error');
@@ -33,67 +33,64 @@ class HomeState extends State<Home> {
   }
 
   @override
-  Widget build (BuildContext context) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        
         title: Text(widget.title),
       ),
       drawer: Drawer(
         child: ListView(
           children: [
-             DrawerHeader(
+            DrawerHeader(
               padding: EdgeInsets.zero,
               decoration: const BoxDecoration(
                 color: Colors.blue,
               ),
               child: Image.asset('images/logo.jpeg', fit: BoxFit.fill),
-              ),
-              ListTile(
-                leading: const Icon(Icons.airplanemode_active),
-                title: const Text('Lugares'),
-                selected: _selectedIndex == 0,
-                onTap: (){
-                  updateSelectedIndex(0);
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.savings),
-                title: const Text('Ofertas'),
-                selected: _selectedIndex == 1,
-                onTap: (){
-                  updateSelectedIndex(1);
-                },
-              ),
-              
-              ListTile(
-                leading: const Icon(Icons.corporate_fare),
-                title: const Text('Hoteles'),
-                selected: _selectedIndex == 2,
-                onTap: (){
-                  updateSelectedIndex(2);
-                },
-              ),
-              const Divider(
-              ),
-              ListTile(
-                leading: const Icon(Icons.airplane_ticket),
-                title: const Text('Mis vuelos'),
-                selected: _selectedIndex == 3,
-                onTap: (){
-                  updateSelectedIndex(3);
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.favorite),
-                title: const Text('Favoritos'),
-                selected: _selectedIndex == 3,
-                onTap: (){
-                  updateSelectedIndex(3);
-                },
-              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.airplanemode_active),
+              title: const Text('Lugares'),
+              selected: _selectedIndex == 1,
+              onTap: () {
+                updateSelectedIndex(1);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.savings),
+              title: const Text('Ofertas'),
+              selected: _selectedIndex == 0,
+              onTap: () {
+                updateSelectedIndex(0);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.corporate_fare),
+              title: const Text('Hoteles'),
+              selected: _selectedIndex == 2,
+              onTap: () {
+                updateSelectedIndex(2);
+              },
+            ),
+            const Divider(),
+            ListTile(
+              leading: const Icon(Icons.airplane_ticket),
+              title: const Text('Mis vuelos'),
+              selected: _selectedIndex == 3,
+              onTap: () {
+                updateSelectedIndex(3);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.favorite),
+              title: const Text('Favoritos'),
+              selected: _selectedIndex == 3,
+              onTap: () {
+                updateSelectedIndex(3);
+              },
+            ),
           ],
-          ),
+        ),
       ),
       body: _getDrawerItemWidget(_selectedIndex),
     );
--- lib/lugares.dart
+++ lib/lugares.dart
@@ -5,8 +5,86 @@ class Lugares extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Hello lugares'),
-    );
+    return Scaffold(
+        body: ListView.builder(
+            itemCount: 5,
+            itemBuilder: (context, index) {
+              return Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(0)),
+                child: Column(
+                  children: <Widget>[
+                    Image.network(
+                      "https://www.peru.travel/Contenido/Home/Imagen/en/12/1.9/Banner/start-en-ds.jpg",
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                "Cancun",
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color(0xff2E2E2E)),
+                              ),
+                              const Text(
+                                "ida y vuelta",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black54),
+                              ),
+                              Container(
+                                decoration: BoxDecoration(
+                                    color:
+                                        const Color.fromARGB(253, 26, 0, 131),
+                                    borderRadius: BorderRadius.circular(60.0)),
+                                child: IconButton(
+                                  onPressed: () {
+                                    print("favoritos");
+                                  },
+                                  icon: const Icon(Icons.favorite_border),
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              const Text(
+                                "s./345.99",
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color(0xff2E2E2E)),
+                              ),
+                              const Text(
+                                "del 03/03 al 14/03",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black54),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  print("ver vuelo");
+                                },
+                                child: const Text("Ver vuelo"),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }));
   }
 }
