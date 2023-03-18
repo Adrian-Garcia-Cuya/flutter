70751f88b1969695f4ca8bad8820d87291f12161
keres101 <keres.kgb@gmail.com>
2023-03-17 21:13:50 -0500

    margenes y separacion entre las cartas

--- lib/lugares.dart
+++ lib/lugares.dart
@@ -6,7 +6,12 @@ class Lugares extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ListView.builder(
+      body: Container(
+        margin: const EdgeInsets.only(left: 10, right: 10, top: 20, bottom: 20),
+        child: ListView.separated(
+            separatorBuilder: (BuildContext context, int index) {
+              return const SizedBox(height: 20);
+            },
             itemCount: 5,
             itemBuilder: (context, index) {
               return Card(
@@ -25,19 +30,26 @@ class Lugares extends StatelessWidget {
                           Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              const Text(
-                                "Cancun",
-                                style: TextStyle(
+                              Container(
+                                margin: const EdgeInsets.only(bottom: 10),
+                                child: const Text(
+                                  "Cancun",
+                                  style: TextStyle(
                                     fontSize: 20,
                                     fontWeight: FontWeight.bold,
-                                    color: Color(0xff2E2E2E)),
+                                    color: Color(0xff2E2E2E),
+                                  ),
+                                ),
                               ),
-                              const Text(
-                                "ida y vuelta",
-                                style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black54),
+                              Container(
+                                margin: const EdgeInsets.only(bottom: 10),
+                                child: const Text(
+                                  "ida y vuelta",
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black54),
+                                ),
                               ),
                               Container(
                                 decoration: BoxDecoration(
@@ -57,19 +69,25 @@ class Lugares extends StatelessWidget {
                           Column(
                             crossAxisAlignment: CrossAxisAlignment.end,
                             children: [
-                              const Text(
-                                "s./345.99",
-                                style: TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                    color: Color(0xff2E2E2E)),
+                              Container(
+                                margin: const EdgeInsets.only(bottom: 10),
+                                child: const Text(
+                                  "s./345.99",
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color(0xff2E2E2E)),
+                                ),
                               ),
-                              const Text(
-                                "del 03/03 al 14/03",
-                                style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black54),
+                              Container(
+                                margin: const EdgeInsets.only(bottom: 10),
+                                child: const Text(
+                                  "del 03/03 al 14/03",
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black54),
+                                ),
                               ),
                               ElevatedButton(
                                 onPressed: () {
@@ -85,6 +103,8 @@ class Lugares extends StatelessWidget {
                   ],
                 ),
               );
-            }));
+            }),
+      ),
+    );
   }
 }
